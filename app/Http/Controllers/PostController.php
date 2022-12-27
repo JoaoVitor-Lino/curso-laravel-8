025c987a15e4505aabd9e2cfff0ae50e28f0afeb
joaovitor <lino.joaovitor12@gmail.com>
2022-12-27 14:37:30 -0300

    Estudos ate a aula 9

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdatePost;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::get();
+        
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create() {
+        return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request) {
+        
+        Post::create($request->all());
+        
+        return redirect()->route('posts.index');
+    }
+    
+    public function show($id) {
+        //$post = Post::where('id', $id)->first();
+        if(!$post = Post::find($id)) { //Se nao tiver ou o id for errado, retorne para pagina inicial
+            return redirect()->route('posts.index');
+        }
+        
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id) {
+        if(!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+            
+        } 
+        $post->delete();
+        return redirect()->route('posts.index')
+                         ->with('message', 'Post Deletado com Sucesso');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+PostController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/create',[PostController::class, 'create'])->name('posts.create');
+Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
+
 
 Route::get('/', function () {
     return view('welcome');
